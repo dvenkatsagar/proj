7f097c57b7c6ad50c846063d0ecc10a2282242a7
Sagar Dwibhashyam <dvenkatsagar@gmail.com>
2016-04-11 15:16:42 -0400

    Fixes 12

--- boneAR/CT3D-Showing-AR-Model_files/threex.webcamgrabbing.js
+++ boneAR/CT3D-Showing-AR-Model_files/threex.webcamgrabbing.js
@@ -1,8 +1,8 @@
 var THREEx = THREEx || {}
 
 // shim
-//navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-//window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
 
 /**
  * Grab camera
@@ -77,29 +77,34 @@ THREEx.WebcamGrabbing = function(){
         }, 500)
 
         // get the media sources
-        navigator.mediaDevices.enumerateDevices().then(function(devices){
-          var constraints = {
-                  video: true,
-                  audio: false,
-          }
-          for (var i = 0; i != devices.length; ++i) {
-                  var sourceInfo = devices[i];
-                  if(sourceInfo.kind == "videoinput") {
-                          constraints.video = {
-                                  facingMode: {exact:"environment"}
-                          }
-                          // not to mirror the video element when it is 'environment'
-                          // domElement.style.transform   = ''
-                  }
-          }
+        MediaStreamTrack.getSources(function(sourceInfos) {
+                // define getUserMedia() constraints
+                var constraints = {
+                        video: true,
+                        audio: false,
+                }
+                // to mirror the video element when it isnt 'environment'
+                // domElement.style.transform   = 'scaleX(-1)'
 
-          // try to get user media
-          navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
-                  domElement.src = URL.createObjectURL(stream);
-          }, function(error) {
-                  console.error("Cant getUserMedia()! due to ", error);
-          });
+                // it it finds the videoSource 'environment', modify constraints.video
+                for (var i = 0; i != sourceInfos.length; ++i) {
+                        var sourceInfo = sourceInfos[i];
+                        if(sourceInfo.kind == "video" && sourceInfo.facing == "environment") {
+                                constraints.video = {
+                                        optional: [{sourceId: sourceInfo.id}]
+                                }
+                                // not to mirror the video element when it is 'environment'
+                                // domElement.style.transform   = ''
+                        }
+                }
 
+                // try to get user media
+                navigator.getUserMedia( constraints, function(stream){
+                        domElement.src = URL.createObjectURL(stream);
+                }, function(error) {
+                        console.error("Cant getUserMedia()! due to ", error);
+                });
         });
+
 	this.domElement = domElement
 }
